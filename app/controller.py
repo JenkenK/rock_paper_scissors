b3f67af06515e762ab4cbec562264f7ed67dfd54
JenkenK <jenkenkwok@gmail.com>
2020-09-25 16:37:25 +0100

    added game logic

--- app/controller.py
+++ app/controller.py
@@ -6,18 +6,17 @@ from app.models.add_player_choice import *
 
 @app.route('/')
 def index():
-    return render_template('index.html', players=player_choice)
+    return render_template('index.html')
 
-@app.route('/add-player', methods=['POST'])
+@app.route('/add-player-to-game', methods=['POST'])
 def add_player_choice():
     player_1 = Player(name=request.form['player_1_name'], choice=request.form['player_1_choice'])
     player_2 = Player(name=request.form['player_2_name'], choice=request.form['player_2_choice'])
     add_player_choice_to_screen(player_1, player_2)
-    return redirect('/')
-
-
+    game = Game()
+    result = game.play(player_1, player_2)
+    return render_template('index.html', players=player_choice, game_result=result)
 
 @app.route('/welcomepage')
 def welcome_page():
     return render_template('welcome_page.html')
-
