33aee4ea6b2ad59b9c13b4bf6945fd57df5f5e48
JenkenK <jenkenkwok@gmail.com>
2020-09-25 23:30:38 +0100

    refactored html files and controller, added css files for welcome page and base

--- app/controller.py
+++ app/controller.py
@@ -7,25 +7,31 @@ game = Game()
 
 @app.route('/')
 def index():
-    return render_template('index.html', players=game.player_choice, game_result=game.result)
+    return render_template('welcome_page.html')
 
-@app.route('/game', methods=['POST'])
+@app.route('/play')
+def play():
+    return render_template('play_game.html', players=game.player_choice, game_result=game.result)
+
+@app.route('/play-game', methods=['POST'])
 def add_player_choice():
     player_1 = Player(name=request.form['player_1_name'], choice=request.form['player_1_choice'])
     player_2 = Player(name=request.form['player_2_name'], choice=request.form['player_2_choice'])
     game.add_player_choice(player_1, player_2)
     game.play(player_1, player_2)
-    return redirect('/')
+    return redirect('/play')
 
 @app.route('/reset', methods=['POST'])
 def reset_game():
     game.reset_game()
-    return redirect('/')
+    return redirect('/play')
 
-@app.route('/welcomepage')
-def welcome_page():
-    return render_template('welcome_page.html')
+@app.route('/rules')
+def rules():
+    return render_template('rules.html')
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
