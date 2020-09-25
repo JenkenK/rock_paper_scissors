194053f58f6eefb0942f73470b6d216524ce75b2
JenkenK <jenkenkwok@gmail.com>
2020-09-25 22:39:55 +0100

    refactored game and controller with reset button

--- app/controller.py
+++ app/controller.py
@@ -2,21 +2,30 @@ from app import app
 from flask import render_template, request, redirect
 from app.models.game import *
 from app.models.player import *
-from app.models.add_player_choice import *
+
+game = Game()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', players=game.player_choice, game_result=game.result)
 
-@app.route('/add-player-to-game', methods=['POST'])
+@app.route('/game', methods=['POST'])
 def add_player_choice():
     player_1 = Player(name=request.form['player_1_name'], choice=request.form['player_1_choice'])
     player_2 = Player(name=request.form['player_2_name'], choice=request.form['player_2_choice'])
-    add_player_choice_to_screen(player_1, player_2)
-    game = Game()
-    result = game.play(player_1, player_2)
-    return render_template('index.html', players=player_choice, game_result=result)
+    game.add_player_choice(player_1, player_2)
+    game.play(player_1, player_2)
+    return redirect('/')
+
+@app.route('/reset', methods=['POST'])
+def reset_game():
+    game.reset_game()
+    return redirect('/')
 
 @app.route('/welcomepage')
 def welcome_page():
     return render_template('welcome_page.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
--- app/models/game.py
+++ app/models/game.py
@@ -1,22 +1,36 @@
 class Game():
+
+    def __init__(self):
+        self.result = ''
+        self.player_choice = []
+
     def play(self, player_1, player_2):
         if player_1.choice == "Rock" and player_2.choice == "Rock":
-            return "Draw!"
+            self.result = "Draw!"
         elif player_1.choice == "Rock" and player_2.choice == "Paper":
-            return player_2.name + " wins!"
+            self.result = player_2.name + " wins!"
         elif player_1.choice == "Rock" and player_2.choice == "Scissors":
-            return player_1.name + " wins!"
+            self.result = player_1.name + " wins!"
         if player_1.choice == "Paper" and player_2.choice == "Paper":
-            return "Draw!"
+            self.result = "Draw!"
         elif player_1.choice == "Paper" and player_2.choice == "Scissors":
-            return player_2.name + " wins!"
+            self.result = player_2.name + " wins!"
         elif player_1.choice == "Paper" and player_2.choice == "Rock":
-            return player_1.name + " wins!"
+            self.result = player_1.name + " wins!"
         if player_1.choice == "Scissors" and player_2.choice == "Scissors":
-            return "Draw!"
+            self.result = "Draw!"
         elif player_1.choice == "Scissors" and player_2.choice == "Rock":
-            return player_2.name + " wins!"
+            self.result = player_2.name + " wins!"
         elif player_1.choice == "Scissors" and player_2.choice == "Paper":
-            return player_1.name + " wins!"
+            self.result = player_1.name + " wins!"
 
+    def add_player_choice(self, player_1,player_2):
+        self.player_choice.clear()
+        self.player_choice.append(player_1)
+        self.player_choice.append(player_2)
+
+
+    def reset_game(self):
+        self.player_choice.clear()
+        self.result = ''
     
